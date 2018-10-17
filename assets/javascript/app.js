f9f49e46c37a15a9cb05a78cef708f0b18ec8851
ShermanTheCat <40065490+mjkelley354@users.noreply.github.com>
2018-10-17 08:33:33 -0400

    condensed profile to single page and styled with semantic-ui

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+
+$(".about-menu").on("click", function() {
+
+});
+
+$(".contact-menu").on("click", function() {
+
+});
+
+$(".portfolio-menu").on("click", function() {
+
+});
+
+$('.ui.accordion')
+    .accordion()
+;
+
+$('.ui.card')
+    .popup({
+        inline: true
+    })
+;
+
